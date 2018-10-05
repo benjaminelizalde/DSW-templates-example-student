e96b864943420d5483dcad567d2b65f3ccfc3d1a
benjaminelizalde <42652971+benjaminelizalde@users.noreply.github.com>
2018-10-05 10:50:48 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,13 +6,21 @@ app = Flask(__name__)
 def render_main():
     return render_template('home.html')
 
-@app.route("/test")
+@app.route("/page1")
 def render_page1():
     return render_template('page1.html')
 
-@app.route("/test2")
+@app.route("/page2")
 def render_page2():
     return render_template('page2.html')
+
+
+
+@app.route("/page3")
+def render_page3():
+    return render_template('page3.html')
+
+
     
 if __name__=="__main__":
     app.run(debug=False, port=54321)
